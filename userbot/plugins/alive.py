8648362e44bb13c4768a493f34603335b781f1c0
AnonMux <64081922+AnonMux@users.noreply.github.com>
2020-04-21 18:16:10 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,13 +6,13 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("`Currently Alive!` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
+                     "`Bot created by:` [Hacker & Cracker](tg://user?id=646154002), @M4_ANONYMOUS\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "https://github.com/Dark-Princ3/X-tra-Telegram")
